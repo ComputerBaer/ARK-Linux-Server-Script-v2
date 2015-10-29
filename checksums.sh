aa2a970177f4a63e32008516283a63836766e232
ComputerBaer <computerbaer@gmail.com>
2015-10-29 16:24:08 +0100

    Removed 'checksums.sh' from Script Updater

--- checksums.sh
+++ checksums.sh
@@ -3,9 +3,13 @@
 echo -n "Generating new Checksums ... "
 
 # Base Directory Scripts
-md5sum arkserver.sh checksums.sh > checksums
+# md5sum arkserver.sh checksums.sh > checksums
+# End users do not need checksums.sh and developers use git
+md5sum arkserver.sh > checksums
+
 # Sample Configuration File
 md5sum configuration-sample.ini >> checksums
+
 # Other Script Files
 md5sum .script/*.sh >> checksums
 
