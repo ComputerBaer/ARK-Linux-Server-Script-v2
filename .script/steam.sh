874755d7c680fafc288430bcec81fb03d199e98f
ComputerBaer <computerbaer@gmail.com>
2015-10-30 12:37:00 +0100

    InstallSteam

--- .script/steam.sh
+++ .script/steam.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# WaitForBackgroundProcess Function
+# Param1 - ProcessID to wait on
+# Param2 - Color of the dots (Optional)
 function WaitForBackgroundProcess
 {
-    PROCESS_ID=$1
+    local PROCESS_ID=$1
+
+    local COLOR=$FG_WHITE
+    if [ ! -z $2 ]; then
+        COLOR=$2
+    fi
 
     while true; do
         if kill -0 $PROCESS_ID 2>/dev/null; then
-            echo -ne "."
+            echo -ne "${COLOR}.${RESET_ALL}"
         else
             break
         fi
@@ -16,6 +24,32 @@ function WaitForBackgroundProcess
     echo # Line break
 }
 
+# InstallSteam Function
+function InstallSteam
+{
+    local STEAMCMD_FILE="${SCRIPT_TEMP_DIR}steamcmd_linux.tar.gz"
+
+    if [ -d $STEAM_CMD_DIR ]; then
+        return
+    fi
+    echo -e "${FG_YELLOW}${STR_STEAM_INSTALL}${RESET_ALL}"
+
+    echo -ne "${FG_YELLOW}${STR_STEAM_DOWNLOAD}${RESET_ALL}"
+    curl -s https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz -o $STEAMCMD_FILE &
+    WaitForBackgroundProcess $! $FG_YELLOW
+
+    mkdir -p $STEAM_CMD_DIR
+    cd $STEAM_CMD_DIR
+
+    echo -ne "${FG_YELLOW}${STR_STEAM_EXTRACT}${RESET_ALL}"
+    tar -xzf $STEAMCMD_FILE &
+    WaitForBackgroundProcess $! $FG_YELLOW
+
+    cd $SCRIPT_BASE_DIR
+    echo -e "${FG_GREEN}${STR_STEAM_SUCCESSFULL}${RESET_ALL}"
+}
+
+# ParseSteamAcf Function
 function ParseSteamAcf
 {
     local path=$1
